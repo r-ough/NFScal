4a51f84755ede9cafb08247376cc85d15376dc1e
Raphael <e11806918@student.tuwien.ac.at>
2020-12-21 14:38:14 +0100

    Initial commit of project structure

---
+++ src/main/java/systems/buffer/nfscal/NfScalApplication.java
@@ -0,0 +1,13 @@
+package systems.buffer.nfscal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfScalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfScalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/systems/buffer/nfscal/NfScalApplicationTests.java
@@ -0,0 +1,13 @@
+package systems.buffer.nfscal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfScalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
